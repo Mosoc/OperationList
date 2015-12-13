6015dc74b54d0c4d45c69fa64bea9ca15f0c8293
MOSOC <warshightide@gmail.com>
2015-12-13 15:18:31 +0000

    Hello World

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
-var express = require("express")
-var app = express()
-var mongojs = require("mongojs")
+var express = require("express");
+var app = express();
+//var mongojs = require("mongojs")
+app.get('/', function(req, res){
+    res.send("Hello World")
+});
+
+app.listen(process.env.PORT, process.env.IP);//8080, 0.0.0.0 
+
+console.log("Server running...");
