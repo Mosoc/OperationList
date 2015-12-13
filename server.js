7984926d62e923735072f5f21ccf552ddf3c84af
MOSOC <warshightide@gmail.com>
2015-12-13 16:06:27 +0000

    add index

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require("express");
 var app = express();
 //var mongojs = require("mongojs")
-app.get('/', function(req, res){
-    res.send("Hello World")
-});
-
+app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT, process.env.IP);//8080, 0.0.0.0 
 
 console.log("Server running...");
