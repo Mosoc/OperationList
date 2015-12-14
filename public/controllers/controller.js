314766395f8c5450336845acebb03e4ca80115be
MOSOC <warshightide@gmail.com>
2015-12-14 16:54:36 +0000

    Test of operationlist controller part1

---
+++ public/controllers/controller.js
@@ -0,0 +1,22 @@
+var myApp = angular.module('myApp', []); 
+myApp.controller('AppCtrl', ['$scope', '$http', 
+function($scope, $http) { console.log("Hello World from controller");
+    operation1 = {
+        title: 'hekate',
+        type: 'A',
+        description: 'A-A-A'
+    };
+    operation2 = {
+        title: 'odin',
+        type: 'B',
+        description: 'B-22-2B2'
+    };
+    
+    operation3 = {
+        title: 'inanna',
+        type: 'C',
+        description: '333-333-333'        
+    };
+    var operationlist = [operation1, operation2, operation3];
+    $scope.operationlist = operationlist;
+}]); 
