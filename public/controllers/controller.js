63e049ffdd93fea4a258c6aed2ac0f7441bcb357
MOSOC <warshightide@gmail.com>
2015-12-14 17:27:37 +0000

    Test of operationlist controller part2

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,23 +1,9 @@
 var myApp = angular.module('myApp', []); 
 myApp.controller('AppCtrl', ['$scope', '$http', 
 function($scope, $http) { console.log("Hello World from controller");
-$http.get('/operationlist');
-    operation1 = {
-        title: 'hekate',
-        type: 'A',
-        description: 'A-A-A'
-    };
-    operation2 = {
-        title: 'odin',
-        type: 'B',
-        description: 'B-22-2B2'
-    };
-    
-    operation3 = {
-        title: 'inanna',
-        type: 'C',
-        description: '333-333-333'        
-    };
-    var operationlist = [operation1, operation2, operation3];
-    $scope.operationlist = operationlist;
+$http.get('/operationlist').success(function(response){
+    console.log("RESPONSE")
+    $scope.operationlist = response;
+});
+
 }]); 
--- server.js
+++ server.js
@@ -4,6 +4,25 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.get('/operationlist', function(req,res){
     console.log("GET request");
+    
+    operation1 = {
+        title: 'hekate',
+        type: 'A',
+        description: 'A-A-A'
+    };
+    operation2 = {
+        title: 'odin',
+        type: 'B',
+        description: 'B-22-2B2'
+    };
+    
+    operation3 = {
+        title: 'inanna',
+        type: 'C',
+        description: '333-333-333'        
+    };
+    var operationlist = [operation1, operation2, operation3];
+    res.json(operationlist);
 });
 app.listen(process.env.PORT, process.env.IP);//8080, 0.0.0.0 
 
